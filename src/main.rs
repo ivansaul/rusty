b0bfc53d2b380d4b2d1e0790627da0633915a0cc
ivansaul <saul_2503@hotmail.com>
2025-09-03 01:45:21 -0500

    feat: add new feature

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,6 @@ async fn daemon() -> eyre::Result<()> {
     let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(5));
     loop {
         interval.tick().await;
-        info!("Daemon is running...");
+        info!("Daemon is running....");
     }
 }
